997dee6f74fbaa3a5a903c91b328ccb602c48e20
wb-ws182862 <wb-ws182862@alibaba-inc.com>
2017-02-07 11:10:26 +0800

    初始化demo

---
+++ SubModule/SubModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubModule
+//
+//  Created by ws on 17/2/7.
+//  Copyright © 2017年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SubModule/SubModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubModule
+//
+//  Created by ws on 17/2/7.
+//  Copyright © 2017年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
